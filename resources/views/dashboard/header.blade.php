ee5148e9192a86c2ee4665f1c4de1161b9979864
luthfitabey <37507037+luthfitabey@users.noreply.github.com>
2023-12-28 15:21:17 +0700

    fixing vite error

--- resources/views/dashboard/header.blade.php
+++ resources/views/dashboard/header.blade.php
@@ -56,12 +56,12 @@
       @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="btn btn-success text-sm text-white-700 dark:text-white-500 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="btn-book-a-table" href="#book-a-table">Log in</a>
+                        <a href="{{ route('login') }}" class="btn btn-primary" href="#book-a-table">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="btn btn-warning text-sm text-white-700 dark:text-white-500 underline">Register</a>
                         @endif
                     @endauth
                 </div>
